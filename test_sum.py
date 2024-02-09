2979ebc89cc80dc00c743f29126c2ceb3ade100d
Aissatou Diallo <ashitadiallo1996@gmail.com>
2024-02-09 11:07:57 +0100

    suprr

--- test_sum.py
+++ test_sum.py
@@ -1,7 +1,9 @@
 # sum.py
 from sum import add
+
+
 def test_add():
- assert add(2, 3) == 5
- assert add(-1, 1) == 0
- assert add(-1, -1) == -2
- print("Test ok !")
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+    print("Test ok !")
